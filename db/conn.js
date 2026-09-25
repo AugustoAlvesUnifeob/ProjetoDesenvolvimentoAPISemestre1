ac88646b58538fd3e71329c62c390f59b7c8ca95
Augusto Alves <augusto.alves@sou.unifeob.edu.br>
2026-09-25 15:58:08 -0300

    Add files via upload

---
+++ db/conn.js
@@ -0,0 +1,22 @@
+//requerer o siquelize
+const Sequelize = require('sequelize')
+
+//parametros de conexão
+const conn = new Sequelize(
+    'db_get_a_pet',
+    'root',
+    '12345678',{
+        host:'localhost',
+        dialect:'mysql',
+        port:3306
+    }
+)
+
+try {
+    conn.authenticate()
+    console.info('Banco de Dados conectado com sucesso!')
+} catch (error) {
+    console.info(`Não foi possivel conectar ao banco:,${error}`)
+}
+
+module.exports = conn
